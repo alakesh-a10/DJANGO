2ab0fa03ef8171500b0ff581551f3cdc75d19813
Alakx <icvc.a10@gmail.com>
2020-05-10 20:12:18 +0530

    First dJango App

--- web_apps/first_app/views.py
+++ web_apps/first_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1 align=center>Hello, world.This is my first Django Application.</h1>")
