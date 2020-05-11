5fe8354ffa8c4c69a9e588233a4a29a179eea969
Alakx <icvc.a10@gmail.com>
2020-05-11 18:35:26 +0530

    commit

--- web_apps/first_app/views.py
+++ web_apps/first_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1 align=center>Hello, world.This is my first Django Application.</h1>")
+    return HttpResponse("<h1 align=center>Hello world.This is my first Django Application.</h1>")
