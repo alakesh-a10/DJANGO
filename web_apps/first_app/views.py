23e34078ca199eaf7367115fa126cd38d1c33660
Alakx <icvc.a10@gmail.com>
2020-05-11 19:52:44 +0530

    template Creation

--- web_apps/first_app/views.py
+++ web_apps/first_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    my_dict={'insert_me':'This line is printed from Views.py'}
+    return render(request,'first_app/index.html',context=my_dict)
     return HttpResponse("<h1 align=center>Hello world.This is my first Django Application.</h1>")
