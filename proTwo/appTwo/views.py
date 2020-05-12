92e5741cb709025d1e4f914eca3bd00478925874
Alakx <icvc.a10@gmail.com>
2020-05-12 19:05:45 +0530

    template challange

--- proTwo/appTwo/views.py
+++ proTwo/appTwo/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>My second App</em")
+
+''' Task to create a help page'''
+def help(request):
+    return render(request,'appTwo/help.html')
